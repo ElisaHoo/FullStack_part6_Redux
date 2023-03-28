76516fdd37ef1757e6841a231911ade50d14e328
Elisa <elisa.harjunpaa@student.hamk.fi>
2023-03-28 09:55:44 +0300

    JSON Server backend, get & post are working (6.14-6.15)

--- redux-anecdotes/src/components/AnecdoteForm.js
+++ redux-anecdotes/src/components/AnecdoteForm.js
@@ -1,15 +1,17 @@
 import { useDispatch } from "react-redux"
 import { createAnecdote } from "../reducers/anecdoteReducer"
 import { createNotification, noNotification } from "../reducers/notificationReducer"
+import anecdoteService from '../services/anecdotes'
 
 const NewAnecdote = (props) => {
     const dispatch = useDispatch()
 
-    const addAnecdote = (event) => {
+    const addAnecdote = async (event) => {
         event.preventDefault()
         const content = event.target.newAnec.value  // referred to input-fields name
         event.target.newAnec.value = ''
-        dispatch(createAnecdote(content))
+        const newAnecdote = await anecdoteService.createNew(content)
+        dispatch(createAnecdote(newAnecdote))
         dispatch(createNotification(`You added '${content}'`))
         setTimeout(() => {
             dispatch(noNotification())
